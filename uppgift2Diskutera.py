1bd9978307a64153e4c78d567404faa471c02237
PiaGu <pia.gunnarsson@student.nbi-handelsakademin.se>
2025-01-10 18:45:19 +0100

    third commit

---
+++ uppgift2Diskutera.py
@@ -0,0 +1,12 @@
+#Biljettpris, räknar ut pengar kvar, samt hälften av pengarna kvar.
+
+biljettpris = 100  # biljettpris
+pengarfickan = 200  # pengar på fickan
+pengarkvar = pengarfickan-biljettpris # Pengar kvar
+halvakvar= pengarkvar/2 #Hälften av summan som är kvar
+
+print("Du har " +str(pengarfickan) + " kronor på fickan.")
+print("Biljetten kostar " +str(biljettpris) + " kronor.")
+print("Det blir " +str(pengarkvar) + " kronor kvar efter köpet.")
+print("Hälften av de " +str(pengarkvar) +" kronorna blir "+str(halvakvar) + " kronor.")
+
